f1c83eb9d144aab5f7e9ef29d4a34164facf3f94
Yulia Alekseenko <Yulia_Alekseenko@epam.com>
2018-08-23 11:17:37 +0300

    add realization of memoize function

--- src/memoize.js
+++ src/memoize.js
@@ -1,3 +1,18 @@
-export function memoize() {
-    // TODO
+export function memoize(func) {
+    const Cache = new Map();
+    if (typeof func !== 'function') return null;
+
+    const memoized = function (...args) {
+        const key = args.map(x => `${String(x)}:${typeof x}`).join('|');
+
+        if (Cache.has(key)) {
+            return Cache.get(key);
+        }
+        const result = func.apply(this, args);
+        Cache.set(key, result);
+        return result;
+    };
+
+    return memoized;
 }
+
